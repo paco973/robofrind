2df0d0426ade3773739642278dfb9e65958dd09d
Paco <djopaco973@gmail.com>
2020-08-25 19:41:13 +0200

    Updates

---
+++ precache-manifest.91f625c04dc658b8f85d1a5b83648524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc1b5922922ac142a77bf145b2c4cef",
+    "url": "/robofrind/index.html"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robofrind/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "260e7b3a55df48d9eeaa",
+    "url": "/robofrind/static/css/main.cadb375f.chunk.css"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robofrind/static/js/2.907bdc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofrind/static/js/2.907bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260e7b3a55df48d9eeaa",
+    "url": "/robofrind/static/js/main.43b1cf43.chunk.js"
+  },
+  {
+    "revision": "d50b9f1cc2a42f551b22",
+    "url": "/robofrind/static/js/runtime-main.5c9f6dab.js"
+  }
+]);
